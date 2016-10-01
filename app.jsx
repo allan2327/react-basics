40c98df12eaf77674472e1983eb3298879b7a666
James Cool <jeffreysbrother@gmail.com>
2016-10-01 11:52:31 -0700

    mockup complete

--- app.jsx
+++ app.jsx
@@ -2,8 +2,25 @@
 // components start with a capital letter
 function Application() {
   return (
-    <div className="application">
-      
+    <div className="scoreboard">
+      <div className="header">
+        <h1>Scoreboard</h1>
+      </div>
+
+      <div className="players">
+        <div className="player">
+          <div className="player-name">
+            James Cool
+          </div>
+          <div className="player-score">
+            <div className="counter">
+              <button className="counter-action decrement"> - </button>
+              <div className="counter-score"> 31 </div>
+              <button className="counter-action increment"> + </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }

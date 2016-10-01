fabbbfc7b5e9eb63c6d929ea7d2c8e05c97c14f9
James Cool <jeffreysbrother@gmail.com>
2016-10-01 14:59:32 -0700

    get rid of bunch of nonsense

--- app.jsx
+++ app.jsx
@@ -35,38 +35,19 @@ Header.propTypes = {
 };
 
 
-// the Counter Stateless Functional Component has been converted into a Component Class...this allows us to add state to the component
-// Any Stateless Functional Component can be written as a Component Class, but the former is typically preferable unless state is needed
-var Counter = React.createClass({
-  propTypes: {
-    initialScore: React.PropTypes.number.isRequired,
-  },
-  getInitialState: function() {
-    return {
-      score: this.props.initialScore,
-    }
-  },
-  incrementScore: function(e) {
-    this.setState({
-      score: (this.state.score + 1),
-    })
-  },
-  decrementScore: function(e) {
-    this.setState({
-      score: (this.state.score - 1),
-    })
-  },
-  render: function() {
-    return (
-      <div className="counter">
-        <button className="counter-action decrement" onClick={this.decrementScore}> - </button>
-        <div className="counter-score"> {this.state.score} </div>
-        <button className="counter-action increment" onClick={this.incrementScore}> + </button>
-      </div>
-    );
-  }
-});
-
+function Counter(props) {
+  return (
+    <div className="counter">
+      <button className="counter-action decrement"> - </button>
+      <div className="counter-score"> {props.score} </div>
+      <button className="counter-action increment"> + </button>
+    </div>
+  );
+}
+Counter.propTypes = {
+  score: React.PropTypes.number.isRequired,
+  onChange: React.PropTypes.func.isRequired,
+}
 
 
 function Player(props) {
@@ -76,7 +57,7 @@ function Player(props) {
         {props.name}
       </div>
       <div className="player-score">
-        <Counter initialScore={props.score} />
+        <Counter score={props.score} />
       </div>
     </div>
   );
@@ -88,30 +69,39 @@ Player.propTypes = {
 
 
 
-// components start with a capital letter
-function Application(props) {
-  return (
-    <div className="scoreboard">
-      <Header title={props.title} />
+var Application = React.createClass({
+  propTypes: {
+    title: React.PropTypes.string,
+    initialPlayers: React.PropTypes.arrayOf(React.PropTypes.shape({
+      name: React.PropTypes.string.isRequired,
+      score: React.PropTypes.number.isRequired,
+      id: React.PropTypes.number.isRequired,
+    })).isRequired,
+  },
+  getDefaultProps: function() {
+    return {
+        title: "Scoreboard",
+    }
+  },
+  getInitialState: function() {
+    return {
+      players: this.props.initialPlayers,
+    };
+  },
+  render: function() {
+    return (
+      <div className="scoreboard">
+        <Header title={this.props.title} />
 
-      <div className="players">
-        {props.players.map(function(player) {
-          return <Player name={player.name} score={player.score} key={player.id}/>
-        })}
+        <div className="players">
+          {this.state.players.map(function(player) {
+            return <Player name={player.name} score={player.score} key={player.id}/>
+          })}
+        </div>
       </div>
-    </div>
-  );
-}
-// optional: .isRequired as a chained method after .string or .number
-Application.propTypes = {
-  title: React.PropTypes.string,
-  players: React.PropTypes.arrayOf(React.PropTypes.shape({
-    name: React.PropTypes.string.isRequired,
-    score: React.PropTypes.number.isRequired,
-    id: React.PropTypes.number.isRequired,
-  })).isRequired,
-};
-Application.defaultProps = {
-  title: "Scoreboard",
-}
-ReactDOM.render(<Application players={PLAYERS}/>, document.getElementById('container'));
+    );
+  }
+});
+
+
+ReactDOM.render(<Application initialPlayers={PLAYERS}/>, document.getElementById('container'));

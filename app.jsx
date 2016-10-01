815438d568f435498d0519c0df08aaec830735bc
James Cool <jeffreysbrother@gmail.com>
2016-10-01 16:39:32 -0700

    add stopwatch section and remove outline from button

--- app.jsx
+++ app.jsx
@@ -24,6 +24,21 @@ var nextId = 5;
 
 
 
+var Stopwatch = React.createClass({
+  render: function() {
+    return (
+      <div className="stopwatch">
+        <h2>Stopwatch</h2>
+        <div className="stopwatch-time">0</div>
+        <button>Start</button>
+        <button>Stop</button>
+      </div>
+    );
+  }
+});
+
+
+
 var AddPlayerForm = React.createClass({
   propTypes: {
     onAdd: React.PropTypes.func.isRequired,
@@ -86,6 +101,7 @@ function Header(props) {
     <div className="header">
       <Stats players={props.players}/>
       <h1>{props.title}</h1>
+      <Stopwatch />
     </div>
   );
 }

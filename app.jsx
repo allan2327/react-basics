90247ff24388258163c541553d76c7ad34718dc0
James Cool <jeffreysbrother@gmail.com>
2016-10-01 12:20:28 -0700

    decomposing

--- app.jsx
+++ app.jsx
@@ -1,27 +1,68 @@
+function Header(props) {
+  return (
+    <div className="header">
+      <h1>{props.title}</h1>
+    </div>
+  );
+}
+Header.propTypes = {
+  title: React.PropTypes.string.isRequired,
+};
+
+
+
+function Counter(props) {
+  return (
+    <div className="counter">
+      <button className="counter-action decrement"> - </button>
+      <div className="counter-score"> {props.score} </div>
+      <button className="counter-action increment"> + </button>
+    </div>
+  );
+}
+Counter.propTypes = {
+  score: React.PropTypes.number.isRequired,
+}
+
+
+
+function Player(props) {
+  return (
+    <div className="player">
+      <div className="player-name">
+        {props.name}
+      </div>
+      <div className="player-score">
+        <Counter score={props.score} />
+      </div>
+    </div>
+  );
+}
+Player.propTypes = {
+  name: React.PropTypes.string.isRequired,
+  score: React.PropTypes.number.isRequired,
+}
+
+
 
 // components start with a capital letter
 function Application(props) {
   return (
     <div className="scoreboard">
-      <div className="header">
-        <h1>{props.title}</h1>
-      </div>
+      <Header title={props.title} />
 
       <div className="players">
-        <div className="player">
-          <div className="player-name">
-            James Cool
-          </div>
-          <div className="player-score">
-            <div className="counter">
-              <button className="counter-action decrement"> - </button>
-              <div className="counter-score"> 31 </div>
-              <button className="counter-action increment"> + </button>
-            </div>
-          </div>
-        </div>
+        <Player name="James C." score={32} />
+        <Player name="John Stamos" score={666} />
       </div>
     </div>
   );
 }
-ReactDOM.render(<Application title="My Scoreboard"/>, document.getElementById('container'));
+// optional: .isRequired as a chained method after .string or .number
+Application.propTypes = {
+  title: React.PropTypes.string,
+};
+Application.defaultProps = {
+  title: "Scoreboard",
+}
+ReactDOM.render(<Application />, document.getElementById('container'));
